2b63cef771a3ba9abcb1dcf2e3fab8320dd24947
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-25 22:42:29 +0100

    clock

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ let select = document.getElementById('select')
 let select2 = document.getElementById('select2')
 let select3 = document.getElementById('select3')
 
-// let hour = document.getElementById("hour")
-// let minute = document.getElementById("minute")
-// let second = document.getElementById("second")
-
-
 for (let index = 0; index <= 23; index++) {
     select.innerHTML += `<option value="${index}"> ${index} </option>`
 }
@@ -14,6 +9,28 @@ for (let index = 0; index <= 60; index++) {
     select2.innerHTML += `<option value="${index}"> ${index} </option>`
 }
 
+function setAlarm() {
+    date = new Date()
+    hour = date.getHours()
+    minute = date.getMinutes()
+    hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
+}
+
+date = new Date()
+setInterval(() => {
+    date = new Date()
+    hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
+
+    if (select.value == hour && select2.value == minute) {
+        alert("It is working")
+    }
+}, 1000);
+
+
+
+// let hour = document.getElementById("hour")
+// let minute = document.getElementById("minute")
+// let second = document.getElementById("second")
 
 // setInterval(() => {
 //     let date = new Date()
@@ -30,24 +47,3 @@ for (let index = 0; index <= 60; index++) {
 //     second.innerHTML = date.getSeconds()
 //     // hour.innerHTML = `<h1>${hour} : ${minute} : ${second}</h1>`
 // }, 1000);
-
-
-
-date = new Date()
-setInterval(() => {
-    date = new Date()
-    hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
-
-}, 1000);
-
-function setAlarm() {
-    hour = date.getHours()
-    minute = date.getMinutes()
-    second = date.getSeconds()
-
-    if (select.value == hour && select2.value == minute) {
-        alert("equal")
-    }else{
-        alert("!equal")
-    }
-}
