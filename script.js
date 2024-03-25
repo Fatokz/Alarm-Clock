74b7c48e748b47c27d8a7c8e5c1b54503f82bcab
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-25 23:11:56 +0100

    working alarm clock

--- script.js
+++ script.js
@@ -9,41 +9,35 @@ for (let index = 0; index <= 60; index++) {
     select2.innerHTML += `<option value="${index}"> ${index} </option>`
 }
 
-function setAlarm() {
-    date = new Date()
-    hour = date.getHours()
-    minute = date.getMinutes()
-    hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
-}
 
 date = new Date()
+// setInterval(() => {
+//     date = new Date()
+//     hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
+// }, 1000);
+
+function setAlarm() {
+    date = new Date()
+    hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
+
+    if (select.value == '' && select2.value == '')  {
+        alert("Set an alarm")
+    }
+    // setInterval(() => {
+    //     date = new Date()
+    //     hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
+
+    //     if (select.value == date.getHours() && select2.value == date.getMinutes()) {
+    //         alert("It is working")
+    //     }
+    // }, 1000);
+}
+
 setInterval(() => {
     date = new Date()
     hour.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} ${select3.value}`
 
-    if (select.value == hour && select2.value == minute) {
+    if (select.value == date.getHours() && select2.value == date.getMinutes()) {
         alert("It is working")
     }
 }, 1000);
-
-
-
-// let hour = document.getElementById("hour")
-// let minute = document.getElementById("minute")
-// let second = document.getElementById("second")
-
-// setInterval(() => {
-//     let date = new Date()
-//     hour.innerHTML = date.getHours()
-//     minute.innerHTML = date.getMinutes()
-//     second.innerHTML = date.getSeconds()
-//     // time.innerHTML = `${hour} : ${minutes} : ${seonds} ` 
-// }, 1000);
-
-// setInterval(() => {
-//     date = new Date()
-//     hour.innerHTML = date.getHours()
-//     minute.innerHTML = date.getMinutes()
-//     second.innerHTML = date.getSeconds()
-//     // hour.innerHTML = `<h1>${hour} : ${minute} : ${second}</h1>`
-// }, 1000);
